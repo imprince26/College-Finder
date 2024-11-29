a867c403d33f3283fd5985752383f2ff1f31bcf8
imprince26 <princepatel260806@gmail.com>
2024-11-30 00:19:32 +0530

    initial work

---
+++ README.md
@@ -0,0 +1,21 @@
+# College Finder
+
+## Description
+College Finder is a web application that allows users to search for universities based on the country name. It fetches data from the Hipolabs Universities API and displays the list of universities in a user-friendly interface.
+
+## Features
+- Search for universities by entering a country name.
+- Clear the search results with a single click.
+- Responsive design that adjusts to different screen sizes.
+
+## Technologies Used
+- HTML
+- CSS
+- JavaScript
+- Axios (for API requests)
+- Font Awesome (for icons)
+
+## Installation
+1. Clone the repository:
+   ```bash
+   git clone <repository-url>
---
+++ app.js
@@ -0,0 +1,36 @@
+let url = "http://universities.hipolabs.com/search?name=";
+let btn = document.querySelector("#Search");
+let clear = document.querySelector("#clear");
+let list = document.querySelector("#list");
+
+btn.addEventListener("click", async ()=>{
+    let country = document.querySelector("input").value;
+    console.log(country);
+
+    let colArr = await getColleges(country);
+        show(colArr);
+    
+});
+clear.addEventListener("click",()=>{
+  reset();
+});
+function reset(){
+    list.innerText ="";
+};
+function show(colArr){
+     list.innerText="";
+    for(col of colArr){
+        let li = document.createElement("li");
+        li.innerText = col.name;
+        list.appendChild(li);
+    }
+};
+async function getColleges(country) {
+    try{
+        let res = await axios.get(url + country);
+        return res.data;
+    }catch(e){
+        console.log("error :",e);
+        return [];
+    }
+};
