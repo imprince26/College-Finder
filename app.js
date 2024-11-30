aeb0cadc08a10a9ed73e5c237cbffa710101e676
imprince26 <princepatel260806@gmail.com>
2024-11-30 11:13:04 +0530

    update style.css

--- app.js
+++ app.js
@@ -3,34 +3,33 @@ let btn = document.querySelector("#Search");
 let clear = document.querySelector("#clear");
 let list = document.querySelector("#list");
 
-btn.addEventListener("click", async ()=>{
-    let country = document.querySelector("input").value;
-    console.log(country);
+btn.addEventListener("click", async () => {
+  let country = document.querySelector("input").value;
+  console.log(country);
 
-    let colArr = await getColleges(country);
-        show(colArr);
-    
+  let colArr = await getColleges(country);
+  show(colArr);
 });
-clear.addEventListener("click",()=>{
+clear.addEventListener("click", () => {
   reset();
 });
-function reset(){
-    list.innerText ="";
-};
-function show(colArr){
-     list.innerText="";
-    for(col of colArr){
-        let li = document.createElement("li");
-        li.innerText = col.name;
-        list.appendChild(li);
-    }
-};
+function reset() {
+  list.innerText = "";
+}
+function show(colArr) {
+  list.innerText = "";
+  for (col of colArr) {
+    let li = document.createElement("li");
+    li.innerText = col.name;
+    list.appendChild(li);
+  }
+}
 async function getColleges(country) {
-    try{
-        let res = await axios.get(url + country);
-        return res.data;
-    }catch(e){
-        console.log("error :",e);
-        return [];
-    }
-};
+  try {
+    let res = await axios.get(url + country);
+    return res.data;
+  } catch (e) {
+    console.log("error :", e);
+    return [];
+  }
+}
